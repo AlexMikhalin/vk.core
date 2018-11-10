1955062a2a56b652c66b64330513b913ad49a7e5
AlexMikhalin <sashamikhalin2017@yandex.ru>
2018-11-10 14:14:58 +0300

    Add project files.

---
+++ vk.core/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MasterCard.Api.Locations;
+using Microsoft.AspNetCore.Mvc;
+using TestMasterCard;
+
+namespace vk.core.Controllers
+{
+    [Route("api/[controller]/")]
+    public class ValuesController : Controller
+    {
+
+        //Координаты:
+        //59.9397392 северной широты
+        //30.3140793 восточной долготы
+        // GET api/values
+        //[HttpGet("{latitude}-{lontitude}")]
+        public ATMLocations/*IEnumerable<double>*/ GetATMLocationsInfo(/*double latitude, double lontitude*/)
+        {
+            ATMLocations atmTest = MasterCard.Api.Locations.ATMLocationsTest.Get(1, -20);
+            return atmTest/*$"latitude is {latitude} \n lontitude is {lontitude}"*/;
+        }
+
+        
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ vk.core/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MasterCard.Api.Locations;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace vk.core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        ATMLocationsTest atmTest = new ATMLocationsTest();
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
